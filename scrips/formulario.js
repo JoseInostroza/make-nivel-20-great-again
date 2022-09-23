ad450a78659403e06887b00970b2b8ce0e6aea4c
JoseInostroza <87823768+JoseInostroza@users.noreply.github.com>
2022-09-23 12:47:50 -0300

    reorganizacion de estructuras y logicas

--- scrips/formulario.js
+++ scrips/formulario.js
@@ -1,16 +1,13 @@
-import { ordenar } from "./turnos";
-
 //esqueleto de ayudanete de turnos para masters
 //ingreso de datos por formulario
 //guardado de info boton agregar
 //por comodidad tomamos el formulario
-let comenzar = document.getElementById('comenzar');
 //por comodidad tomamos todos los inputs
 let inputs = document.querySelectorAll('#formulario input');
 //aqui tomamos el boto que tiene una funcion dentro de la pagina generalmente un submit
-let agregar = document.getElementById('agregar');
+const agregar = document.getElementById('agregar');
 //tomamos la notificacion de ingreso correcto o incorrecto para trabajarla mas adelante
-let confirmacion = document.getElementById('confirmacion')
+const cierre = document.getElementById('cierre');
 //variable ausiliares que nos ayudara a guardar los nombres de cada objeto 
 var nombre = ''
 var velocidad = ''
@@ -36,31 +33,27 @@ class participante{
     }
 };
 //constantes que nos ayudaran a integrar una inferfaz visual de los participantes actuales
-const alineacion = document.getElementById('alineacion')
-const player = "<i class='bx bx-user'></i>"
-const nonplayer ="<i class='bx bxs-user' ></i>"
+const listaNpc = document.getElementById('npc')
+const listaPlayers = document.getElementById('player')
+const player = "<i class='bx bxs-face'></i>"
+const nonplayer="<i class='bx bx-bot'></i>"
 
 //array para guardar a los partisipantes y luego ordenarlos 
 let turnos = []
 //funcion para validar campos y asegurar que los campos inportantes no esten vacios
 const validar= (input,campo)=>{
     if(input === ''){
-        document.getElementById(`formulario__alerta_${campo}`).classList.remove('inactiva')
-        document.getElementById(`formulario__alerta_${campo}`).classList.add('activa')
+        document.getElementById(`formulario-${campo}`).classList.add('activa')
         validador[campo] = false
     }
     else{
-        document.getElementById(`formulario__alerta_${campo}`).classList.remove('activa')
-        document.getElementById(`formulario__alerta_${campo}`).classList.add('inactiva')
+        document.getElementById(`formulario-${campo}`).classList.remove('activa')        
         validador[campo] = true
     }
 }
-
-agregar.addEventListener('click',(e)=>{
-    e.preventDefault()
+agregar.addEventListener('click',()=>{
     //tengo que probar si funciona sin prevent default si funciona se saca, si no tengo que programar el borrado
     //de las opciones 
-    
     //se rescata la data para trabajar con ella 
     inputs.forEach((dato)=>{
         //esto se puede hacer con un if simple evalua esa idea porque 
@@ -88,27 +81,28 @@ agregar.addEventListener('click',(e)=>{
             break
         }
     });
+
+    //validador de opciones de ingreso 
     if(validador.nombre && validador.velocidad && validador.tipo){
         turnos.push(new participante(nombre,velocidad,estado,tipo))
         confirmacion.innerHTML= `Se agrego correctamente a ${nombre}`;
         if(tipo === 'npc'){
-            alineacion.innerHTML += nonplayer
+            listaNpc.innerHTML += nonplayer
         }else{
-            alineacion.innerHTML += player
+            listaPlayers.innerHTML += player
         }
-        confirmacion.classList.remove('inactiva');
-        confirmacion.classList.add('activa', 'correcto');
+        confirmacion.classList.add('correcto');
         setTimeout(()=>{
-            confirmacion.classList.remove('activa', 'correcto');
-            confirmacion.classList.add('inactiva');
+            confirmacion.classList.remove('correcto');
         },4000)
     }
     else{
         confirmacion.innerHTML='faltan campos mi rey';
-        confirmacion.classList.remove('inactiva');
-        confirmacion.classList.add('activa', 'incorrecto');
+        confirmacion.classList.add('activa');
     }
-
     console.log(turnos)
-    console.log(validador)
 });
+
+cierre.addEventListener('click', ()=>{
+    
+})
--- scrips/turnos.js
+++
@@ -1,8 +0,0 @@
-function ordenar(Aray){
-    Aray.sort((a,b)=>{
-        b.velocidad - a.velocidad
-    })
-    return Aray
-}
-
-export {ordenar}
