68191fdb4d786ce6404d30ea9909f8b831b64315
JoseInostroza <87823768+JoseInostroza@users.noreply.github.com>
2022-10-05 23:15:21 -0300

    estructuras basicas

---
+++ scrips/tiendas.js
@@ -0,0 +1,4 @@
+const traccionData = document.querySelectorAll('#transaccionDirecta', 'input')
+
+let valorCarrito={cobre:0,plata:0,oro:0,platino:0}
+let valorFondo={cobre:0,plata:0,oro:0,pltaino:0}
