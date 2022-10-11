d07d3aa68e29b9a18a744a984952a8dd25d0c4a3
JoseInostroza <87823768+JoseInostroza@users.noreply.github.com>
2022-10-11 00:37:49 -0300

    boton de calcular funcionando

--- scrips/tiendas.js
+++ scrips/tiendas.js
@@ -8,19 +8,31 @@ let listaMonedas = ['Cobre','Plata','Oro','Platino']
 let monto
 let tipo
 let valorCarrito={'Cobre':0,'Plata':0,'Oro':0,'Platino':0}
-let valorFondo={cobre:0,plata:0,oro:0,pltaino:0}
+let valorFondo={'Cobre':0,'Plata':0,'Oro':0,'Platino':0}
 
 function actaulizarCarro(moneda, valor){
     valor= parseInt(valor)
     valorCarrito[moneda]+= valor
     carro.forEach((element)=>{        
-        if(element.id === moneda){
-            console.log(element)            
+        if(element.id === moneda){           
             element.innerHTML = valorCarrito[moneda]
         }
     })
 }
 
+function CalcularVivisas(objeto,lista){
+    for (i=0; i<lista.length; ++i){
+        if(objeto[lista[i]]>9 && lista[i]!=='Platino'){
+           up= objeto[lista[i]]/10;
+           objeto[lista[i+1]] += Math.trunc(up);
+           up=0;
+           objeto[lista[i]] = objeto[lista[i]]%10
+           actaulizarCarro(lista[i],0)
+           actaulizarCarro(lista[i+1],0)
+        }
+        
+    }
+}
 gastoBoton.addEventListener('click',()=>{
     transaccionData.forEach((e)=>{
         switch(e.name){
@@ -37,13 +49,16 @@ gastoBoton.addEventListener('click',()=>{
                 if(listaMonedas.includes(e.value)){
                     switch(e.value){
                         case 'Cobre':
-                            actaulizarCarro(e.value,monto)                            
+                            actaulizarCarro(e.value,monto)                           
                             break
                         case 'Plata':
+                            actaulizarCarro(e.value,monto) 
                             break
                         case 'Oro':
+                            actaulizarCarro(e.value,monto) 
                             break
                         case 'Platino':
+                            actaulizarCarro(e.value,monto) 
                             break
                     }
                 }else{
@@ -53,3 +68,5 @@ gastoBoton.addEventListener('click',()=>{
     })
     
 });
+
+calcular.addEventListener('click',()=>{CalcularVivisas(valorCarrito,listaMonedas)})
