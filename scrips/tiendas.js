8bd65e944d569c32fcfbcbdd3d763749559fb774
JoseInostroza <87823768+JoseInostroza@users.noreply.github.com>
2022-10-15 21:28:19 -0300

    estilos y logica en cobre

--- scrips/tiendas.js
+++ scrips/tiendas.js
@@ -1,4 +1,5 @@
 const gastosData = document.querySelectorAll('#gastos input')
+const fondosData = document.querySelectorAll('#fondos input')
 const carro = document.querySelectorAll('#gastoTotal span')
 const  gastoBoton= document.getElementById('agregarCobro')
 const fondoBoton = document.getElementById('agregarFondo')
@@ -8,7 +9,6 @@ let listaMonedas = ['Cobre','Plata','Oro','Platino']
 let monto
 let tipo
 let valorCarrito={'Cobre':0,'Plata':0,'Oro':0,'Platino':0}
-let valorFondo={'Cobre':0,'Plata':0,'Oro':0,'Platino':0}
 
 function actaulizarCarro(moneda, valor){
     valor= parseInt(valor)
@@ -22,7 +22,7 @@ function actaulizarCarro(moneda, valor){
 
 function CalcularVivisas(objeto,lista){
     for (i=0; i<lista.length; ++i){
-        if(objeto[lista[i]]>9 && lista[i]!=='Platino'){
+        if((objeto[lista[i]]>9 || objeto[lista[i]]<-9) && lista[i]!=='Platino'){
            up= objeto[lista[i]]/10;
            objeto[lista[i+1]] += Math.trunc(up);
            up=0;
@@ -51,16 +51,24 @@ gastoBoton.addEventListener('click',()=>{
                 if(listaMonedas.includes(e.value)){
                     switch(e.value){
                         case 'Cobre':
-                            actaulizarCarro(e.value,monto)                           
+                            actaulizarCarro(e.value,monto)
+                            CalcularVivisas(valorCarrito,listaMonedas)
+                            e.value=''                           
                             break
                         case 'Plata':
-                            actaulizarCarro(e.value,monto) 
+                            actaulizarCarro(e.value,monto)
+                            CalcularVivisas(valorCarrito,listaMonedas)
+                            e.value='' 
                             break
                         case 'Oro':
-                            actaulizarCarro(e.value,monto) 
+                            actaulizarCarro(e.value,monto)
+                            CalcularVivisas(valorCarrito,listaMonedas)
+                            e.value='' 
                             break
                         case 'Platino':
-                            actaulizarCarro(e.value,monto) 
+                            actaulizarCarro(e.value,monto)
+                            CalcularVivisas(valorCarrito,listaMonedas)
+                            e.value='' 
                             break
                     }
                 }else{
@@ -71,8 +79,50 @@ gastoBoton.addEventListener('click',()=>{
     
 });
 
-calcular.addEventListener('click',()=>{CalcularVivisas(valorCarrito,listaMonedas)})
+calcular.addEventListener('click',()=>{
+    listaMonedas.forEach((i)=>{
+        valorCarrito[i]=0
+    })
+})
 
 fondoBoton.addEventListener('click', ()=>{
-
+    fondosData.forEach((e)=>{
+        switch(e.name){
+            case 'monederoNumero':
+                if(e.value !== ''){
+                    monto = -parseInt(e.value) 
+                    e.value=''
+                    break
+                }else {
+                    console.log('aqui va una alerta');
+                    break
+                };
+            case 'monederoMoneda':                
+                if(listaMonedas.includes(e.value)){
+                    switch(e.value){
+                        case 'Cobre':
+                            actaulizarCarro('Cobre',monto)
+                            CalcularVivisas(valorCarrito,listaMonedas)
+                            e.value=''                          
+                            break
+                        case 'Plata':
+                            actaulizarCarro('Cobre',(monto*10))
+                            CalcularVivisas(valorCarrito,listaMonedas)
+                            e.value=''
+                            break
+                        case 'Oro':
+                            actaulizarCarro('Cobre',(monto*100))
+                            CalcularVivisas(valorCarrito,listaMonedas)
+                            e.value=''
+                            break
+                        case 'Platino':
+                            actaulizarCarro('Cobre',(monto*1000))
+                            CalcularVivisas(valorCarrito,listaMonedas)
+                            e.value=''
+                            break
+                    }
+                }else{
+                    console.log('not gucci');
+                }
+}})
 })
